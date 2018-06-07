80773d8b3d359dc8016538693b3d9bbe0be79809
oldestcrab <oldestcrab@gmail>
2018-06-07 17:04:50 +0800

    再次练习

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 import requests
 
-url = "https://www.baiud.com"
+url = "https://www.baidu.com"
 headers = {
     "user-agent":"",
     "connection":"keep-alive"
 }
-kw = {
-    "kw":"baidu"
-}
+kw = {"kw","baidu"}
 formdata = {
     "i":"baidu",
     "email":"qiu@163.com",
@@ -20,17 +18,17 @@ proxy = {
 auth = ("name","password")
 sess = requests.session()
 
-response = requests.get(url, headers = headers, params = kw, proxies = proxy, atuh = auth, verfiy = True)
-responses = sess.post(url, data = formdata, headers = headers)
+responose = requests.get(url, headers = headers, params = kw, proxy = proxies, auth = auth, verify = True)
+responses = requests.post(url, headers = headers, data = formdata)
 
-print(response.text, response.content, response.url, response.encoding, response.status_code)
+print(response.text, response.content, response.url, response.encoding, responses.status_code)
 
 from lxml import etree
 
 html = etree.HTML(text)
 result = html.xpath('/bookstore/book[last()-1]//a[@href = "link.html"]')
 #/a/@href 获取href属性值
-results = html.xpath('/div[contains(@id,"qiu")]')
+results = html.xpath('/div[contains(@id, "qiu")]')
 
 print(result[0].text, results[0].tag)
 
@@ -42,23 +40,23 @@ result = jsonpath.jsonpath(jsonobj, '$..')
 
 import re
 
-pattern = re.compile(r'\d+')
+pattern = re.compile(r'\d+\d?')
 s = "one123two456three789"
-a = pattern.match(s,0,9)
-#search findall finditer
+a = pattern.match(s)
+# search findall finditer
 b = pattern.split(s)
 c = pattern.sub(r'four',s)
 
 #请求报头
 Get https://www.baidu.com/ HTTP/1.1
 Host: www.baidu.com
-Connection: keep-alive
+Connection: Keep-alive
+User-Agent: 
 Upgrade-Insecure-Requests: 1
-User-agent:
 referer:
 Accept:
-Accetp-encoding:
-Accpet-Language:
+Accept-encoding:
+Accept-language:
 Accept-Charset:
 Cookie:
 Content-Type:
