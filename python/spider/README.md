34807e6eaf5f601d9244fc2889b349f16fcd60f2
oldestcrab <oldestcrab@gmail>
2018-06-08 17:41:25 +0800

    新增爬虫多线程

--- python/spider/README.md
+++ python/spider/README.md
@@ -4,3 +4,4 @@
 
 1. `request模块.md`——Python模块request的简单用法
 2. `absolute.md`——爬虫应该烂熟于心的一些基础知识
+3. `threading.py`——爬虫多线程示例
---
+++ python/spider/threading.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+import threading
+from Queue import Queue
+import requests
+from lxml import etree
+import json
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
