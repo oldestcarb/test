bbd6c40027845983333f61942a8c39d662169894
oldestcrab <oldestcrab@gmail>
2018-06-11 09:06:59 +0800

    早上练习

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import requests
 url = "https://www.baidu.com"
 headers = {
     "user-agent":"",
-    "connnection":"keep-live"
+    "connection":"keep-alive"
 }
-kw = {"kw","baidu"}
+kw = {"kw":"baidu"}
 formdata = {
-    "i","baidu",
+    "i":"baidu",
     "email":"qiu@163.com",
     "pwd":"123456"
 }
@@ -15,48 +15,47 @@ proxy = {
     "http":"http://1.1.1.1:1111",
     "https":"https://2.2.2.2:2222"
 }
-auth = ("name":"password")
+auth = ("name","password")
 sess = requests.session()
 
-resposne = requests.get(url, headers = headers, params = kw, proxies = proxy, auth = auth, verify = True)
-resposnes = sess.post(url, data = formdata, headers = headers)
+response = requests.get(url, params = kw, headers = headers, proxies = proxy, auth = auth, verify = True)
+responses = sess.post(url, data = formdata, headers = headers)
 
-print(response.text, response.content, response.url, response.encoding, response.status_code)
+print(response.text, response.content, response.url, response.status_code, response.encoding)
 
 from lxml import etree
 
 html = etree.HTML(text)
 result = html.xpath('/bookstore/book[last()-1]//a[@href = "link.html"]')
-#/a/@href 获取href属性值
+# /a/@href 获取href属性值
 results = html.xpath('/div[contains(@id,"qiu")]')
 
-print(result[0].text, results[0].tag)
+print(result[0].text, result[0].tag)
 
 import json
 import jsonpath
 
-jsonobj = json.loads(text)
-result = jsonpath.jsonpath(jsonobj,'$..')
+josnobj = json.loads(text)
+result = jsonpath.jsonpath(josnobj, '$..')
 
 import re
 
-pattern = re.compile(r'\d+\d?')
+pattern = re.compile(r'\d?\d+')
 s = "one123two456three789"
-a = pattern.match(s,1,9)
-# search findall finditer spilt
-#b = pattern.spilt(s)
-c = pattern.sub(r'four',s)
+a = pattern.match(s,0,9)
+# search findall finditer split
+b = pattern.sub(r'four',s)
 
-#请求报头
-get https://www.baidu.com/ http/1.1
+# 请求报头
+get https://www.baidu.com http/1.1
 host: www.baidu.com
 connection: keep-alive
-upgrade-secure-requests: 1
+upgrade-insecure-requests: 1
 user-agent:
 accept:
 referer:
 accept-encoding:
-accept-language:
+accept-lanuage:
 accept-charset:
 cookie:
 content-type:
