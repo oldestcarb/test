07f8b2a522eed76a5fab98e6dd0655aa96bfd9d4
oldestcrab <oldestcrab@gmail.com>
2018-06-20 23:55:48 +0800

    单独页面已完成

--- execute/test.py
+++ execute/test.py
@@ -6,9 +6,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from lxml import etree
+import json
 #import os
 
-browser = webdriver.PhantomJS(executable_path='C:/Users/crab/Desktop/mybooks/execute/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+browser = webdriver.PhantomJS(executable_path=r'C:/Users/CRAB/Desktop/mybooks/execute/phantomjs-2.1.1-windows/bin/phantomjs.exe')
 
 
 wait = WebDriverWait(browser, 10)
@@ -23,8 +24,9 @@ def index_page(page):
     try:
         url = 'http://paper.sciencenet.cn/paper/fieldlist.aspx?id=2'
         browser.get(url)
-        '''
+        
         if page > 1:
+            print('right')
             input = wait.until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, '#AspNetPager1.highlight2 input[name = "AspNetPager1_input"]')))
             submit = wait.until(
@@ -32,13 +34,13 @@ def index_page(page):
             input.clear()
             input.send_keys(page)
             submit.click()
-        wait.until(
-            EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#AspNetPager1.highlight2 font[class = "red"]'), str(page)))
+        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#AspNetPager1.highlight2 font[class = "red"]'), str(page)))
         #wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
-        '''
+        
         get_products()
     except TimeoutException:
-        index_page(page)
+        #index_page(page)
+        print(EOFError)
 
 
 def get_products():
@@ -49,18 +51,18 @@ def get_products():
     #with open('./html.html', 'a', encoding = 'utf-8') as f:
     #    f.write(html)
     result = etree.HTML(html)
-    items = result.xpath('//div[@id = "mleft3"]//tbody')
-    print(items)
-    print(type(items))
+    items = result.xpath('//*[@id="DataGrid1"]/tbody//tbody')
+    #print(items)
+    #print(type(items))
     for item in items:
-        print(type(item))
-        title = item.xpath('.//td')
-        print(title)
+        #print(type(item))
+        #title = item.xpath('./tr[2]/td[1]/a')[0].text
+        #print(title,type(title))
         content = {
-            #'title': item.xpath('./td')[0].text
-            #'url': item.xpath('./td/a/@href')[0].text(),
-            #'from': item.xpath('./td')[1].text(),
-            #'date': item.xpath('./td')[2].text()
+            'title': item.xpath('.//td[@width = "70%"]/a')[0].text.replace('\n','').replace(' ',''),
+            'url': item.xpath('.//td[@width = "70%"]/a/@href')[0],
+            'from': item.xpath('.//td[@width = "20%"]')[0].text.replace('\n','').replace(' ',''),
+            'date': item.xpath('.//td[@width = "10%"]')[0].text.replace('\n','').replace(' ','')
         }
         print(content)
         save_to_json(content)
@@ -72,15 +74,17 @@ def save_to_json(result):
     :param result: 结果
     """
     with open('./result.json', 'a', encoding = 'utf-8') as f:
-        f.write(json.dumps(result, ident = 2, ensure_ascii = False))
+        f.write(json.dumps(result, indent = 2, ensure_ascii = False))
     
 
 def main():
     """
     遍历每一页
     """
-    for i in range(1, 2):
+    for i in range(1, 4):
+        print(i)
         index_page(i)
+        
     browser.close()
 
 
