78e22f38c2ef38b1cd2f2be577c49d90f36ff11c
oldestcrab <oldestcrab@gmail.com>
2018-07-02 22:30:19 +0800

    修改一点错误

--- python/README.md
+++ python/README.md
@@ -7,3 +7,4 @@
 3. `essential.md`——爬虫应该烂熟于心的一些基础知识
 4. `threading.py`——爬虫多线程示例
 5. `test.py`——练习脚本
+6. `tool/`——常用工具
--- python/test.py
+++ python/test.py
@@ -3,47 +3,41 @@ import requests
 url = 'https://www.baidu.com'
 kw = {'kw':'baidu'}
 formdata = {
-    'i','biadu',
-    'email','qiu@163.com',
-    'pwd','123456'
+    'i':'baidu',
+    'email':'qiu@163.com',
+    'pwd':'123456'
 }
-headers = {
-    'user-agent':'',
-    'connection':'keep-alive'
-}
+headers = {'user-agent':'', 'connection':'keep-alive'}
 proxy = {
     'http':'http://1.1.1.1:1111',
     'https':'https://2.2.2.2:2222'
 }
-auth = ('name','password')
-
+auth = ('name', 'password')
 sess = requests.session()
 
 response = requests.get(url, params = kw, headers = headers, proxies = proxy, auth = auth, verify = False)
-responses = sess.post(url, data = formdata, headers = headers)
-print(resposne.text,response.content, response.url, response.encoding, response.status_code)
+responses = requests.post(url, data = formdata, headers = headers)
+
+print(resposne.text, response.content, resposne.encoding, response.url, response.status_code)
 
 from lxml import etree
 
 html = etree.HTML(text)
 
-result = html.xpath('bookstore/book[last()-1]//a[@href = "link.html"]')[0].text
+result = html.xpth('bookstore/book[last()-1]//a[contains(@id, "qiu") and @href = "link.html"]')[0].text
 # /a/@href 获取href属性值
-results = html.xpah('/div[contains(@id, 'qiu')]')[0].text
 
 import json
 import jsonpath
 
 jsonobj = json.loads(text)
-result = jsonpath.jsonpath(jsonobj,'$..')
+result = jsonpath.jsonpath('$..')
 
-import re
+list = []
 
-pattern = re.compile('\d\w',re.S)
-a = 'one123two456three789'
-result = pattern.match(a, 0, 9)
-# search, findall, finditer, split
-results = pattern.sub(r'four', s)
+with open('a.json', 'a', encoding = 'utf-8') as f:
+    f.write(json.dumps(list, indent = 2, ensure_ascii = False))
+
 
 # 请求报头
 get https://www.baidu.com/ http/1.1
@@ -54,16 +48,19 @@ user-agent:
 accept:
 referer:
 accept-encoding:
-accept-language:
-accpet-setchar:
-cookie:
+accept-langauge:
+accept-chatset:
+cookies:
 content-type:
 
-import json 
+import re
 
-items = {}
-with open('test.json', 'w', encoding = 'utf-8') as f:
-    f.write(json.dumps(items, ident = 2, ensure_ascii = False))
+pattern = re.compile(r'\s\d+', re.S)
+a = 'one123two456three789'
+b = pattern.match(a, 0, 9)
+# search, findall, finditer, split
+c = pattern.sub(r'four', a)
+
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -72,14 +69,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome()
 browser.implicitly_wait(10)
-browser.get("https://www.baidu.com")
+browser.get('https://www.baidu.com')
 input = browser.find_element(By.ID, 'q')
-list = browser.find_elements(By.CSS_SELECTOR,'service li')
+list = browser.find_elements(By.CSS_SELECTOR, 'service li')
 wait = WebDriverWait(browser, 10)
 inputs = wait.until(EC.presence_of_element_located(By.ID, 'q'))
 input.clear()
 input.send_keys('baidu')
 input.click()
 print(browser.page_source, input.text, input.get_attribute('class'))
-# input.id, size, location, tag_name
+
 browser.close()
