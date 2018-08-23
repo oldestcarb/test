b9a038f7580bf2eac20d4da49397ef2052a288da
oldestcrab <oldestcrab@gmail>
2018-08-23 16:49:53 +0800

    增加数据保存在mysql

--- python/essential.md
+++ python/essential.md
@@ -147,3 +147,60 @@ print(browser.page_source, input.text, input.get_attribute('class'))
 # input.id, size, location, tag_name
 browser.close()
 ```
+
+
+### mysql
+
+#### 插入和更新|主键不存在便插入数据，存在则更新数据
+```python
+import pymysql
+
+db = pymysql.connect(host='localhost', user='root',password='123456',port=3306,db='bmnars')
+cursor = db.cursor()
+data = {
+    'id': '20120001',
+    'name': 'Bob',
+    'age': 21
+}
+
+table = 'students'
+keys = ', '.join(data.keys())
+values = ', '.join(['%s'] * len(data))
+
+sql = 'INSERT INTO {table}({keys}) VALUES ({values}) ON DUPLICATE KEY UPDATE'.format(table=table, keys=keys, values=values)
+update = ','.join([" {key} = %s".format(key=key) for key in data]) + ';'
+sql += update
+try:
+    if cursor.execute(sql, tuple(data.values())*2):
+        print('Successful')
+        db.commit()
+except:
+    print('Failed')
+    db.rollback()
+cursor.close()
+db.close()
+
+```
+#### 删除和查询
+```python
+# 删除
+table = 'students'
+condition = 'age > 20'
+sql = 'DELETE FROM  {table} WHERE {condition};'.format(table=table, condition=condition)
+
+# 查询
+sql = 'SELECT * FROM students WHERE age >= 20;'
+try:
+    cursor.execute(sql)
+    print('Count:', cursor.rowcount)
+    row = cursor.fetchone()
+    while row:
+        print('Row:', row)
+        row = cursor.fetchone()
+except:
+    print('Error')
+```
+
+
+
+
--- python/test.py
+++ python/test.py
@@ -1,65 +1,3 @@
-import requests
-
-url = 'https://www.baidu.com/s/'
-headers = {
-    'user-agent':'',
-    'connection':''
-}
-kw = {'kw','baidu'}
-formdata = {
-    'i':'baidu',
-    'email':'qiu@163.com',
-    'password':'123456'
-}
-proxy = {
-    'http':'http://1.1.1.1:1111',
-    'https':'https://2.2.2.2:2222' 
-}
-auth('name','password')
-sess = requests.session()
-
-response = requests.get(url, headers = headers, params = kw, proxies = proxy, auth = auth, verify = False)
-responses = sess.post(url, data = formdata, headers = headers)
-
-print(response.text, response.content, response.url, response.encoding, response.status_code)
-
-from lxml import etree
-
-html = etree.HTML(text)
-result = html.xpath('/bookstore/book[last()-1]//a[contains(@id, "qiu") and @href = "link.html"]')[0].text
-
-import json
-import jsonpath
-
-jsonobj = json.loads(text)
-result = jsonpath.jsonpath(jsonobj, '$..')
-
-list = []
-with open('a.json', 'a', encoding = 'utf-8') as f:
-    f.write(json.dumps(list, indent = 2, ensure_ascii = False))
-
-get https://www.baidu.com/ http/1.1
-host: www.baidu.com
-connection: keep-alive
-upgrade-insecure-requests: 1
-user-agent:
-accept:
-referer:
-accept-encoding:
-accept-language:
-accept-charset:
-cookies:
-content-type:
-
-
-import re
-
-pattern = re.compile(r'\s\d+.*', re.S)
-a = 'one123two456three789'
-b = pattern.match(a, 0, 9)
-# search, findall, finditer, split
-c = pattern.sub(r'four', a)
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -67,16 +5,53 @@ from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome()
 browser.implicitly_wait(10)
-browse.get('https://www.baidu.com')
+browser.get('https://www.baidu.com')
 input = browser.find_element(By.ID, 'q')
-list = browser.find_elements(By.CSS_SELECTOR, 'servie li')
+
+list = browser.find_elements(By.CSS_SELECTOR, 'li')
 wait = WebDriverWait(browser, 10)
-inputs = wait.until(EC.presence_of_element_located(By.ID, 'q'))
+input = wait.until(EC.presence_of_element_located(By.ID, 'q'))
 input.clear()
 input.send_keys('baidu')
 input.click()
-print(browser.page_source, input.text, input.get_attribute('class')
-)
-
-
+print(browser.page_source, input.text, input.get_attribute('class'))
 browser.close()
+
+# mysql
+
+import pymysql
+
+db = pymysql.connect(host='localhost',user='root',password='123456',port=3306.db='bmnars')
+cursor = db.cursor()
+data = {
+    'id':'20180823',
+    'name':'qiu',
+    'age':21
+}
+table = 'students'
+keys = ', 'join(data.keys())
+value = ', 'join(['%s']*len(data))
+sql = 'insert into {talbe}({keys}) values ({valuse}) on duplicat key update'.format(table=table, keys = keys, values = values)
+update = ','.join([" {key} = %s".format(key=key) for key in data])
+sql += update
+try:
+    if cursor.execute(sql,tuple(data.values())*2):
+        db.commit()
+except:
+    db.rollback()
+cursor.close()
+db.close()
+
+table = 'students'
+condition = 'age>2'
+sql = 'delete from {table} where {condition};'.format(table=table,condition=condition)
+
+sql = 'select * from students where age>=20;'
+try:
+    cursor.execute(sql)
+    row = cursor.fetchone()
+    while row:
+        print(row)
+        row = cursor.fetchone()
+except:
+    print('error')
