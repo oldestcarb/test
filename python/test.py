e502c90c03952968c93279b0c266d3dcb2ec261c
oldestcrab <oldestcrab@gmail>
2018-11-12 16:27:08 +0800

    熟能生巧啊弟弟

--- python/test.py
+++ python/test.py
@@ -2,120 +2,78 @@ import requests
 import sys
 import random
 
-url = 'https://www.baidu.com'
+url = 'https:www.baidu.com'
 kw = {'kw':'baidu'}
-with open(sys.path[0] + '/user-agent.txt', 'r', encoding = 'utf-8') as f:
-    list_agents = f.readlines()
-    user_agent = random.choice(list_agents).strip()
-headers = {'user-agent':user_agent, 'cookie':''}
+with open(sys.path[0] + '/user-agents.txt', 'r', encoding = 'utf-8') as f:
+    list_user_agent = f.readlines()
+    user_agent = random.choice(list_user_agent).strip()
+headers = {'user-agent':user_agent, cookie:''}
 formdata = {
     'i':'fanyi',
-    'email':'qiu@163.com',
-    'pwd':'123456' 
+    'user':'qiu',
+    'pwd':'123456'
 }
-proxy = {
-    'http':'http://1.1.1.1:1111',
-    'https':'https://2.2.2.2:2222'
-}
-auth = ('name','password')
+proxiy = {'http':'http://1.1.1.1:1111','https':'https://2.2.2.2:2222'}
+auth = ('qiu','password')
 sess = requests.Session()
-
-response = requests.get(url, headers = headers, params = kw, proxies = proxy, auth = auth, verify = False)
-responses = sess.post(url, headers = headers, data = formdata)
-
-print(response.text, response.content, response.url, response.status_code, response.encoding)
+response = requests.get(url, params = kw, headers = headers, proxies = proxies, auth = auth, verify = False)
+responses = sess.post(url, data = formdata, headers = headers)
+print(response.text, response.content, response.url, response.status_code, response_encoding)
 
 from lxml import etree
 
-result = htmll.xpath('/bookstore//book[last()-1]//a[contains(@id, "qiu") and @href = "link.html"]')[0].text
+text = etree.HTML(html)
+resutl = text.xpath('/bookstore//book[last()-1]//a[contains(@id, "qiu") and @link = "html"]')[0].text
 # @href
 
 import json
 import jsonpath
 
-jsonobj = json.load(html)
-resutl = jsonpath.jsonpath(josnobj,'$..')
+wiht open('a.json', 'w', encoding = 'utf-8') as f:
+    f.write(json.dumps(list, indent = 2, ensure_ascii = False))
+    # json.dump(item, f, ensure_ascii= False)
+
+text = json.loads(thml)
+result = jsonpath.jsonpath(text, '$..')
 
 import re
 
 pattern = re.compile(r'\d+', re.S|re.I)
-s = 'sojglsjlj'
+s = 'one123two345sljdg'
 a = pattern.match(s, 0, 9)
-# search findall finditer split
-b = pattern.sub(r'fousr', s)
+# search, findall, finditer, split
+b = pattern.sub(r'sld', s)
 
-get https://www.baidu.com/ http/1.1
+get https://www.baidu.com http/1.1
 host www.baidu.com
 connection: keep-alive
-upgrade-insecure-requsets: 1
 user-agent:
-accept:
+upgrade-insecure-requests: 1
 referer:
+accept:
 accept-encoding:
-accept-language:
-accept-charset:
-cookie:
+accpet-language:
+accpet-charset:
 content-type:
-
-with open('../result/test.json', 'a', encoding = 'utf-8') as f:
-    f.write(json.dumps(items, indent= 2, ensure_ascii = False))
-    # json.dump(items, f , ensure_ascii = False)
+cookie:
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 browser = webdriver.Chrome(chrome_options=chrome_options)
-browser.implicitly_wait(10)
-broser.get('https://www.baidu.com')
-input = browser.find_element(By.ID, 'q')
-list = browser.find_elements(By.CSS_SELECTOR, 'service-bdli')
+
+browser.get('https://www.baidu.com')
+wait = browser.implicitly_wait(10)
+input = browser.find_element(By.ID, 'qiu')
+list = browser.find_elements(By.CSS_SELECTOR, 'service-q')
 wait = WebDriverWait(browser, 10)
-input = wait.until(EC.presence_of_element_located(By.ID, 'q'))
-input.clear(0)
-input.send_keys('baidu')
-print(browser.page_source, input.text, input.get_attribute('class'))
+subb = wait.until(EC.presence_of_element_located((By.ID 'qiu')))
+input.clear()
+input.send_keys('slgj')
+subb.click()
+print(browser.page_source, input.get_attribute('cll'), input.text)
 browser.close()
-
-import pymysql
-
-db = pymysql.connect(host = 'localhost', user = 'root', password = '123456', port = 3306, db = 'bmnars')
-cursor = db.cursor()
-data = {
-    'id':'202010',
-    'name':'bob',
-    'age':21
-}
-
-table = 'students'
-keys = ', '.join(data.keys())
-values = ', '.join(['%s'] * len(data))
-
-sql = 'insert into {table}({keys} values({values}) on duplicate key update'.format(table = table, keys = keys, values = values)
-update = ','.join([" {keys} = %s".format(key = key) for key in data]) + ';'
-sql += updata
-try:
-    if cursor.execute(sql, tuple(data.values())*2):
-        db.commit()
-except:
-    db.rollback()
-cursor.close()
-db.close()
-
-table = 'students'
-condition = 'age>20'
-sql = 'delete from {table} where {condition};'.format(table = table, condition = condition)
-
-sql = 'select * from students where age>=20;'
-try:
-    cursor.execute(sql)
-    print('count:', cursor.rowcount)
-    row = cursor.fetchone()
-    while row:
-        print('row:', row)
-        row = cursor.fetchone()
-except:
-    print('error')
