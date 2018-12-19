30e22a34f56c0daadabbb83487db3cddf5e6b50e
oldestcrab <oldestcrab@gmail.com>
2018-12-19 22:59:11 +0800

    存储模块完成

--- python/proxypool/proxypool/db.py
+++ python/proxypool/proxypool/db.py
@@ -29,7 +29,79 @@ class RedisClient():
         if not self.db.zscore(REDIS_KEY, proxy):
             return self.db.zadd(REDIS_KEY, {proxy:score})
 
+    def decrease(self, proxy):
+        """
+        代理值减一分，小于最小值则删除
+        :param proxy: 代理
+        :return: 修改后的代理分数
+        """        
+        score = self.db.zscore(REDIS_KEY, proxy)
+        if score and score > MIN_SCORE:
+            print('代理 ', proxy, ' 当前分数：', score, '减１')
+            return self.db.zincrby(REDIS_KEY, -1, proxy)
+        else:
+            print('代理 ', proxy, ' 当前分数：', score, '移除')
+            return self.db.zrem(REDIS_KEY, proxy)
+
+    def exists(self, proxy):
+        """
+        判断代理是否存在
+        :param proxy: 代理
+        :return: 是否存在
+        """            
+        return  not self.db.zscore(REDIS_KEY, proxy) == None
+
+    def max(self, proxy):
+        """
+        将代理设置为MAX_SCORE
+        :param proxy: 代理
+        :return: 设置结果
+        """         
+        print('代理 ', proxy, '可用，设置为 ', MAX_SCORE)
+        return self.db.zadd(REDIS_KEY, {proxy:MAX_SCORE})
+
+    def count(self):
+        """
+        获取数量
+        :return: 数量
+        """       
+        return self.db.zcard(REDIS_KEY)
+
+    def all(self):
+        """
+        获取全部代理
+        :return: 全部代理列表
+        """            
+        return self.db.zrangebyscore(REDIS_KEY, MIN_SCORE, MAX_SCORE)
+
+    def batch(self, start, stop):
+        """
+        批量获取
+        :param start: 开始索引
+        :param stop: 结束索引
+        :return: 代理列表
+        """
+        return self.db.zrevrange(REDIS_KEY, start, stop-1)  
+
+    def random(self):
+        """
+        随机获取有效代理，首先尝试获取最高分数代理，如果不存在，按照排名获取，否则异常
+        :return: 随机代理
+        """
+        result = self.db.zrangebyscore(REDIS_KEY, MAX_SCORE, MAX_SCORE)
+        if len(result):
+            return choice(result)
+        else:
+            result = self.db.zrevrange(REDIS_KEY, 0, 100)
+            if len(result):
+                return choice(result)
+            else:
+                raise PoolEmptyError
+
 if  __name__ == '__main__':
     conn = RedisClient()
-    result = conn.add('192.168.8.2:8888')
+    # result = conn.add('192.168.8.2:8888')
+    result = conn.random()
+    # result = conn.batch(0, 1)
     print(result)
+    # help(RedisClient) 
--- python/proxypool/proxypool/error.py
+++ python/proxypool/proxypool/error.py
@@ -3,4 +3,4 @@ class PoolEmptyError(Exception):
         super(PoolEmptyError, self).__init__()
     
     def __str__(self):
-        return('代理池已枯竭！')
+        return ('代理池已枯竭！')
