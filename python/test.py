432f18c5798057fa0849130fd32c6d9383fde419
oldestcrab <oldestcrab@gmail.com>
2019-09-11 17:27:41 +0800

    增加python基础面试题

--- python/test.py
+++ python/test.py
@@ -1,18 +1,10 @@
-
-import multiprocessing
-import time
- 
-def process(num):
-    time.sleep(num)
-    print('Process:', num)
- 
-if __name__ == '__main__':
-    for i in range(5):
-        p = multiprocessing.Process(target=process, args=(i,))
-        p.start()
- 
-    print('CPU number:' + str(multiprocessing.cpu_count()))
-    for p in multiprocessing.active_children():
-        print('Child process name: ' + p.name + ' id: ' + str(p.pid))
- 
-    print('Process Ended')
+'''
+@Description: In User Settings Edit
+@Author: your name
+@Date: 2019-05-13 11:08:26
+@LastEditTime: 2019-09-11 17:26:10
+@LastEditors: Please set LastEditors
+'''
+a = 'sldjgsljdglsj'
+for i in a.split():
+    print(i + ':' + a.count(i))
