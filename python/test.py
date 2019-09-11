36b83dc8e59a2f556d8e59e9c00a64d210ade7fc
oldestcrab <oldestcrab@gmail.com>
2019-09-11 23:03:32 +0800

    学习到31题

--- python/test.py
+++ python/test.py
@@ -1,10 +1,1 @@
-'''
-@Description: In User Settings Edit
-@Author: your name
-@Date: 2019-05-13 11:08:26
-@LastEditTime: 2019-09-11 17:26:10
-@LastEditors: Please set LastEditors
-'''
-a = 'sldjgsljdglsj'
-for i in a.split():
-    print(i + ':' + a.count(i))
+print('ilovechina'[::-1])
