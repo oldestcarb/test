c5ada4edf999684624ddc7a2f152ce2fd4cfd1a6
oldestcrab <oldestcrab@gmail.com>
2019-09-13 20:07:16 +0800

    暂学习到更新处

--- python/test.py
+++ python/test.py
@@ -1,3 +1,16 @@
-a = ['a','b','c','d']
-for i in enumerate(a, start=8):
-    print(i)
+from lxml import etree
+
+text = '''
+<div>
+    <ul>
+		<li class="item-0"><a href="link1.html">first item</a></li>
+		<li class="item-1"><a href="link2.html">second item</a></li>
+		<li class="item-inactive"><a href="link3.html">third item</a></li>
+		<li class="item-1"><a href="link4.html">fourth item</a></li>
+		<li class="item-0"><a href="link5.html">fifth item</a>
+     </ul>
+ </div>
+'''
+html = etree.HTML(text)
+result = html.xpath('//li')
+print(result)
